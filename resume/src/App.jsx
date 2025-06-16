11a5191685bf6588ba0b9beb9513cf60c235f48f
mesatyamsharma123 <96259558+mesatyamsharma123@users.noreply.github.com>
2025-06-16 07:14:55 +0530

    don

---
+++ resume/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div>
+      <h1>hello from react </h1>
+      <h2>this is my resume</h2>
+     </div> and React logos to learn more
+     
+    </>
+  )
+}
+
+export default App
